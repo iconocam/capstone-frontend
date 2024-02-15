d3e885aaeb2dce3bdfe2a8ad59be71412fdc4871
iconocam <150183122+iconocam@users.noreply.github.com>
2024-02-15 12:01:12 -0500

    succesfully made connection with database, 200 status report on fetching database info

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,32 @@ import './App.css'
 import Home from'./components/Home'
 import About from './pages/About'
 import Contact from'./pages/Contact'
+import { useState, useEffect} from 'react'
 // Is  best practice to just make a home component or do i make a pages folder with home in it?
 
 // http://localhost:5173/home
 // http://localhost:5173/about
 // http://localhost:5173/contact
 
+// querying database to grab information with a useEffect hook
+//Fetch doesn't automatically return JSON so turn data into response.json
 function App() {
-  
+  const [data, setData] = useState([])
 
+  useEffect(() => {
+
+    const getPersonalities = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/personalities')
+        const data = await response.json()
+        setData(data)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    getPersonalities()
+  }, [])
   return (
       <Router>
         <div className="app-container">
